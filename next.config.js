cfb3d5c8d4c4819c4c03a447e557018a306a1a1b
Abdul Haseeb <abdulhaseeb5050@gmail.com>
2024-10-29 05:49:43 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -20,10 +20,4 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
